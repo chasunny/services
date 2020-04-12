39e78e79add39090e08d3768a19b80da7b2bd917
dev <dev@mindtree.com>
2020-04-12 19:44:28 +0530

    kafka ip address

--- food/src/main/java/com/order/my/food/config/KafkaConfiguration.java
+++ food/src/main/java/com/order/my/food/config/KafkaConfiguration.java
@@ -22,7 +22,7 @@ public class KafkaConfiguration {
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> config = new HashMap<>();
 
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.30.152:9092");
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.42.235:9092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(config);
