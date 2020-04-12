fa74c2ec728281e1932269e1f1a9fba49c1d0440
dev <dev@mindtree.com>
2020-04-12 16:23:35 +0530

    test files commented

--- UserManagementService/src/test/java/com/food/my/usermanagement/UsermanagementApplicationTests.java
+++ UserManagementService/src/test/java/com/food/my/usermanagement/UsermanagementApplicationTests.java
@@ -1,13 +1,13 @@
-package com.food.my.usermanagement;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UsermanagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.food.my.usermanagement;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UsermanagementApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
