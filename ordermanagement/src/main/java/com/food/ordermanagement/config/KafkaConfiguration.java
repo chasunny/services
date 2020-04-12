3fb4689b98f81b3b674c426b4842d820bed2c661
dev <dev@mindtree.com>
2020-04-12 19:44:51 +0530

    kafka

--- ordermanagement/src/main/java/com/food/ordermanagement/config/KafkaConfiguration.java
+++ ordermanagement/src/main/java/com/food/ordermanagement/config/KafkaConfiguration.java
@@ -65,7 +65,7 @@ public class KafkaConfiguration {
 	public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory() {
 		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		Map<String, Object> config = new HashMap<>();
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.42.235:9092");
 		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_json");
 		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
